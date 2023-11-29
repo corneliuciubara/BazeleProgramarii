7b4c487f396eff0663a176ad33a91e52fd5bd2be
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2023-11-29 08:26:55 +0200

    Update Laborator9.cpp

--- Laborator9.cpp
+++ Laborator9.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n,i;
     
     // Citim numărul de elemente al vectorului
     cout << "Introduceți numărul de elemente ale vectorului: ";
@@ -13,22 +13,20 @@ int main() {
     
     // Citim elementele vectorului
     cout << "Introduceți " << n << " numere naturale pentru vectorul A:" << endl;
-    for (int i = 0; i < n; ++i) {
+    for (i = 0; i < n; ++i) {
         cin >> A[i];
     }
     
     int pozitie = -1; // Inițializăm cu -1 pentru a indica că nu s-a găsit niciun element egal cu 0
     
     // Căutăm ultimul element egal cu 0 în vector
-    for (int i = n - 1; i >= 0; --i) {
-        if (A[i] == 0) {
-            pozitie = i;
-            break; // Ieșim din buclă după ce am găsit prima apariție a lui 0 în sens invers
-        }
+    for (i = n - 1; (i >= 0) && (A[i] != 0); --i) {
+        // if (A[i] == 0) {
+            // pozitie = i;
     }
     
-    if (pozitie != -1) {
-        cout << "Ultimul element egal cu 0 se află la poziția: " << pozitie << endl;
+    if (i != -1) {
+        cout << "Ultimul element egal cu 0 se află la poziția: " << i+1  << endl;
     } else {
         cout << "Nu s-a găsit niciun element egal cu 0 în vector." << endl;
     }
